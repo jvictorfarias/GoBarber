f298e6290943282094e3eeed854744008d795e67
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2020-04-13 12:25:10 -0300

    creating models

---
+++ src/models/Appointment.ts
@@ -0,0 +1,17 @@
+import { uuid } from 'uuidv4';
+
+class Appointment {
+  id: string;
+
+  provider: string;
+
+  date: Date;
+
+  constructor(provider: string, date: Date) {
+    this.id = uuid();
+    this.provider = provider;
+    this.date = date;
+  }
+}
+
+export default Appointment;
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,14 +1,9 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
 
-const appointmentsRouter = Router();
+import Appointment from '../models/Appointment';
 
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
+const appointmentsRouter = Router();
 
 const appointments: Array<Appointment> = [];
 
@@ -26,11 +21,7 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ error: 'This appointment is already booked' });
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
+  const appointment = new Appointment(provider, parsedDate);
 
   appointments.push(appointment);
 
