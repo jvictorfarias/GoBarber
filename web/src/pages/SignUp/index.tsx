36c2633ec059988168bd8b18b19821e29bdbc67d
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2020-04-23 19:30:47 -0300

    chore: yup form validation

--- web/src/pages/SignUp/index.tsx
+++ web/src/pages/SignUp/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -9,9 +10,23 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .email('Digite email válido')
+          .required('Email obrigatório'),
+        password: Yup.string().min(6, 'Mínimo de 6 dígitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: true,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   return (
     <Container>
