2feb88f6a8d2c58e1cb1fdc488085c507888799a
jvictorfarias <victorfarias.new@gmail.com>
2020-05-17 16:33:19 -0300

    :card_file_box: creating tokens migration and token injection

---
+++ api/src/modules/users/infra/typeorm/repositories/UserTokensRepository.ts
@@ -0,0 +1,32 @@
+import { getRepository, Repository } from 'typeorm';
+import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
+
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+
+class UserTokensRepository implements IUserTokensRepository {
+  private ormRepository: Repository<UserToken>;
+
+  constructor() {
+    this.ormRepository = getRepository(UserToken);
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = await this.ormRepository.findOne({
+      where: { token },
+    });
+
+    return userToken;
+  }
+
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = this.ormRepository.create({
+      user_id,
+    });
+
+    await this.ormRepository.save(userToken);
+
+    return userToken;
+  }
+}
+
+export default UserTokensRepository;
--- api/src/shared/container/index.ts
+++ api/src/shared/container/index.ts
@@ -9,8 +9,8 @@ import AppointmentsRepository from '@modules/appointments/infra/typeorm/reposito
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
-// import IUserTokenRepository from '@modules/users/repositories/IUserTokenRepository';
-// import UserTokenRepository from '@modules/users/infra/typeorm/repositories/UserTo'
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
 container.registerSingleton<IAppointmentsRepository>(
   'AppointmentsRepository',
@@ -20,4 +20,9 @@ container.registerSingleton<IAppointmentsRepository>(
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository,
 );
