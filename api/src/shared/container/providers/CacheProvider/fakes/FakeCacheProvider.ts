40c2bbba42d1d75a52c0f425a76b56873a4c4e2f
jvictorfarias <victorfarias.new@gmail.com>
2020-05-22 00:51:21 -0300

    :clown_face: fake cache provider to run tests

---
+++ api/src/shared/container/providers/CacheProvider/fakes/FakeCacheProvider.ts
@@ -0,0 +1,44 @@
+import ICacheProvider from '../models/ICacheProvider';
+import ICacheProviderDTO from '../dtos/ICacheProviderDTO';
+
+interface ICacheData {
+  [key: string]: string;
+}
+
+class FakeCacheProvider implements ICacheProvider {
+  private cache: ICacheData;
+
+  constructor() {
+    this.cache = {} as ICacheData;
+  }
+
+  public async save({ key, value }: ICacheProviderDTO): Promise<void> {
+    this.cache[key] = JSON.stringify(value);
+  }
+
+  public async recover<T>(key: string): Promise<T | null> {
+    const data = await JSON.parse(this.cache[key]);
+
+    if (!data) {
+      return null;
+    }
+
+    return data as T;
+  }
+
+  public async invalidate(key: string): Promise<void> {
+    delete this.cache[key];
+  }
+
+  public async invalidatePrefix(prefix: string): Promise<void> {
+    const keys = Object.keys(this.cache).filter(key =>
+      key.startsWith(`${prefix}:`),
+    );
+
+    keys.forEach(key => {
+      delete this.cache[key];
+    });
+  }
+}
+
+export default FakeCacheProvider;
