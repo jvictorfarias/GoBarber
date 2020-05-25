a6b19f31c80d5e4a8f5259bcb674aabf36118be1
jvictorfarias <victorfarias.new@gmail.com>
2020-05-24 22:16:19 -0300

    :lipstick: schedule list styles

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,13 +1,52 @@
 import React from 'react';
 
+import { FiClock } from 'react-icons/fi';
 import Header from '../../components/Header';
-import { Container } from './styles';
+import {
+  Container,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Calendar,
+} from './styles';
 
 const Dashboard: React.FC = () => {
   return (
     <Container>
       <Header />
-      <h1>Dashboard</h1>
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars0.githubusercontent.com/u/6394522?s=460&u=fd39696a472492b5c59012836e08524f811a255a&v=4"
+                alt="me"
+              />
+
+              <strong>Joao Victor</strong>
+              <span>
+                <FiClock /> 08:00
+              </span>
+            </div>
+          </NextAppointment>
+          <Section>
+            <strong>Manhã</strong>
+          </Section>
+          <Section>
+            <strong>Tarde</strong>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
--- web/src/pages/Dashboard/styles.ts
+++ web/src/pages/Dashboard/styles.ts
@@ -1,3 +1,110 @@
 import styled from 'styled-components';
 
 export const Container = styled.div``;
+
+export const Content = styled.main`
+  max-width: 1120px;
+  margin: 64px auto;
+  display: flex;
+`;
+
+export const Schedule = styled.div`
+  flex: 1;
+  margin-right: 120px;
+
+  h1 {
+    font-size: 36px;
+  }
+
+  p {
+    margin-top: 8px;
+    color: #ff9000;
+    display: flex;
+    align-items: center;
+    font-weight: 500;
+
+    span {
+      display: flex;
+      align-items: center;
+    }
+
+    span + span::before {
+      content: '';
+      width: 1px;
+      height: 12px;
+      background: #ff9000;
+      margin: 0 8px;
+    }
+  }
+`;
+
+export const NextAppointment = styled.div`
+  margin-top: 64px;
+
+  > strong {
+    color: #999591;
+    font-size: 24px;
+    font-weight: 400;
+  }
+
+  div {
+    background: #3e3b47;
+    display: flex;
+    align-items: center;
+    padding: 16px 24px;
+    border-radius: 10px;
+    margin-top: 24px;
+    position: relative;
+
+    &::before {
+      content: '';
+      position: absolute;
+      height: 80%;
+      width: 1px;
+      left: 0;
+      top: 10%;
+      background: #ff9000;
+    }
+
+    img {
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+    }
+
+    strong {
+      margin-left: 24px;
+      color: #fff;
+    }
+
+    span {
+      margin-left: auto;
+      display: flex;
+      align-items: center;
+      color: #999591;
+
+      svg {
+        color: #ff9000;
+        margin-right: 8px;
+      }
+    }
+  }
+`;
+
+export const Section = styled.div`
+  margin-top: 48px;
+
+  > strong {
+    color: #999591;
+    font-size: 20px;
+    line-height: 26px;
+    border-bottom: 1px solid #3e3b47;
+    display: block;
+    padding-bottom: 16px;
+    margin-bottom: 16px;
+  }
+`;
+
+export const Calendar = styled.aside`
+  width: 380px;
+`;
